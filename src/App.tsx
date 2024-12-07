32c3b8b55609b7165daca04d213c921c254bb074
John Gamboa <john@bizbox.ph>
2024-12-07 10:42:34 +0800

    Initial commit - setup complete

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
